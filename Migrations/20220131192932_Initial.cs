0e597e83aa45970bc0ed12818df4ab8bb7f594d8
Gedas <gedas.malakauskas@gmail.com>
2022-01-31 21:33:50 +0200

    Initial

---
+++ Migrations/20220131192932_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleEntityFrameworkBundle.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services
+            .AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    Environment.GetEnvironmentVariable("ConnectionStrings__Default")
+                    ?? throw new ArgumentNullException("ConnectionStrings__Default"));
+            });
+    })
+    .Build();
+
+await host.RunAsync();
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+
+    [Keyless]
+    public class User
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
